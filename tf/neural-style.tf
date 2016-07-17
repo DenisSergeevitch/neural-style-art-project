dabf519cf73e5f0d36b3fb767d400af321448dae
DenisSergeevitch <i@shir-man.com>
2016-07-17 14:55:46 +0300

    Update neural-style.tf
    Обновление параметров для заказа EC2 инстанса

--- tf/neural-style.tf
+++ tf/neural-style.tf
@@ -1,19 +1,19 @@
 variable "aws_key_name" {}
 variable "access_key" {}
 variable "secret_key" {}
-
-
+ 
+ 
 provider "aws" {
     access_key = "${var.access_key}"
     secret_key = "${var.secret_key}"
-    region = "us-east-1"
+    region = "eu-west-1"
 }
-
+ 
 resource "aws_spot_instance_request" "neural_style_worker" {
-    ami = "ami-07aa916d"
+    ami = "ami-640f9417"
     instance_type = "g2.2xlarge"
-    availability_zone = "us-east-1e"
-    spot_price = "0.25"
+    availability_zone = "eu-west-1a"
+    spot_price = "0.65"
     count = "${var.count}"
     wait_for_fulfillment = "true"
     key_name = "${var.aws_key_name}"
@@ -21,4 +21,3 @@ resource "aws_spot_instance_request" "neural_style_worker" {
         Name = "NeuralStyleWorker"
     }
 }
-
