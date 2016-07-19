a3b86c934c7b71bbeb90d1578c001d24901005da
DenisSergeevitch <i@shir-man.com>
2016-07-19 17:12:19 +0300

    Back to EU
    New neural-art AMI on AWS eu region

--- tf/neural-style.tf
+++ tf/neural-style.tf
@@ -1,18 +1,18 @@
 variable "aws_key_name" {}
 variable "access_key" {}
 variable "secret_key" {}
- 
- 
+
+
 provider "aws" {
     access_key = "${var.access_key}"
     secret_key = "${var.secret_key}"
-    region = "us-east-1"
+    region = "eu-west-1"
 }
 
 resource "aws_spot_instance_request" "neural_style_worker" {
-    ami = "ami-07aa916d"
+    ami = "ami-36c8a845"
     instance_type = "g2.2xlarge"
-    availability_zone = "us-east-1e"
+    availability_zone = "eu-west-1a"
     spot_price = "0.65"
     count = "${var.count}"
     wait_for_fulfillment = "true"
