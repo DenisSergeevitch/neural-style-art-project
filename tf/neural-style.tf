8f52cc973738930c28592729c102f114cf0a7c9d
DenisSergeevitch <i@shir-man.com>
2016-07-19 16:12:07 +0300

    Мы переехали в вирджинию
    Кто-то удалил образ в Ирландии, пришлось вернуться в вирджинию, но там иногда большая очередь на ЕС2

--- tf/neural-style.tf
+++ tf/neural-style.tf
@@ -6,13 +6,13 @@ variable "secret_key" {}
 provider "aws" {
     access_key = "${var.access_key}"
     secret_key = "${var.secret_key}"
-    region = "eu-west-1"
+    region = "us-east-1"
 }
- 
+
 resource "aws_spot_instance_request" "neural_style_worker" {
-    ami = "ami-640f9417"
+    ami = "ami-07aa916d"
     instance_type = "g2.2xlarge"
-    availability_zone = "eu-west-1a"
+    availability_zone = "us-east-1e"
     spot_price = "0.65"
     count = "${var.count}"
     wait_for_fulfillment = "true"
